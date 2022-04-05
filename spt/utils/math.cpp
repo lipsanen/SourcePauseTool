daebae265b6071a123fdf2d21ff5fe7b33b7b7b7
lipsanen <jukspaz@gmail.com>
2022-04-05 20:07:26 +0300

    Added missing includes and build updates

--- spt/utils/math.cpp
+++ spt/utils/math.cpp
@@ -61,9 +61,11 @@ extern "C" {
     double __exp_finite(double x) { return exp(x); }
     double __log_finite(double x) { return log(x); }
     double __pow_finite(double x, double y) { return pow(x, y); }
+	float __atan2_finite(double x, double y) { return atan2(x, y); }
 
     float __expf_finite(float x) { return expf(x); }
     float __logf_finite(float x) { return logf(x); }
     float __powf_finite(float x, float y) { return powf(x, y); }
+	float __atan2f_finite(float x, float y) { return atan2f(x, y); }
 }
 #endif
--- spt/utils/string_utils.hpp
+++ spt/utils/string_utils.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <sstream>
 #include <wchar.h>
 #include <tier0/wchartypes.h>
 
